f75879416106f9fdc661c0f320eedcb1b1042c31
Dvir <dvir019@gmail.com>
2020-01-25 12:27:14 +0200

    Add the template file for question 2

---
+++ hw5q2.py
@@ -0,0 +1,30 @@
+def count_occurrences(s, c):
+    pass
+    # optional signature.
+    # also possible to leave blank
+
+
+
+def are_equal(s1, s2):
+    pass
+    # write your code here
+
+
+def main():
+    print('Enter s1:')
+    s1 = input()
+    print('Enter s2:')
+    s2 = input()
+
+    res = are_equal(s1, s2)
+    if res:
+        print('Strings are equal!')
+    else:
+        print('Strings are not equal...')
+
+
+# leave intact
+if __name__ == '__main__':
+    main()
+
+
